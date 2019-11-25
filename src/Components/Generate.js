c68b1cf3bc6c65d67f17aeabda7e71be6a76508f
Dev Govindji <friendsofdev94@gmail.com>
2019-11-25 14:14:33 -0600

    refactors generate function

--- src/Components/Generate.js
+++ src/Components/Generate.js
@@ -4,46 +4,15 @@ import { adjectives, nouns } from '../words'
 
 
 class Generate extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            hasBeenClicked: false
-        }
-    }
 
-    handleClick = (e) => {
-        this.setState({ hasBeenClicked: true })
-        console.log('clicked!')
 
-    }
-    randomAdjective = (e) => {
+    generate = () => {
         let maxAdjective = adjectives.length
-        let randomNum = Math.floor(Math.random() * Math.floor(maxAdjective))
-        console.log(adjectives[randomNum])
-        let adjective = adjectives[randomNum]
-        return adjective
-    }
-
-    randomNoun = (e) => {
         let maxNoun = nouns.length
-        let randomNum = Math.floor(Math.random() * Math.floor(maxNoun))
-        console.log(nouns[randomNum])
-        let noun = nouns[randomNum]
-        return noun
-
-    }
-    random = (e) => {
-
-        let maxAdjective = adjectives.length
         let adjIdx = Math.floor(Math.random() * Math.floor(maxAdjective))
-        console.log(adjectives[adjIdx])
-        let adjective = adjectives[adjIdx]
-
-        let maxNoun = nouns.length
         let nounIdx = Math.floor(Math.random() * Math.floor(maxNoun))
-        let noun = nouns[nounIdx]
-        console.log(adjective + ' ' + noun)
-        return adjective + ' ' + noun
+        console.log(adjectives[adjIdx] + ' ' + nouns[nounIdx])
+        return adjectives[adjIdx] + ' ' + nouns[nounIdx]
     }
 
 
@@ -53,7 +22,7 @@ class Generate extends React.Component {
 
         return (
             <div className="g-div" >
-                <button onClick={e => { e.preventDefault(); this.handleClick(); this.random() }}>Generate</button>
+                <button onClick={e => { e.preventDefault(); this.generate() }}>Generate</button>
 
             </div>
 
