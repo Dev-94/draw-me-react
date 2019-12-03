05337e7e3fb01d16a5d8a1c57df15df8bea0573c
Dev Govindji <friendsofdev94@gmail.com>
2019-12-02 18:26:35 -0600

    adds to nouns array

--- src/words.js
+++ src/words.js
@@ -1,5 +1,5 @@
 
 let adjectives = ['floppy', 'red', 'crooked']
-let nouns = ['dog', 'turtle', 'crow']
+let nouns = ['dog', 'turtle', 'crow', 'cow', 'sun', 'chair', 'cup', 'tomato', 'backpack', 'tower', 'grape', 'notepad', 'paper', 'pencil', 'car', 'tree', 'fan', 'house', 'person', 'alien', 'boat']
 
 export { adjectives, nouns }
