e1ea5a6f2dcca8366a72ce9d13cee662719c37b6
Dev Govindji <friendsofdev94@gmail.com>
2019-12-18 13:05:01 -0600

    creates nav buttons

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 function Nav() {
+    <button>Home</button>
+        <button>Sign Up</button>
+        <button>Log In</button>
 
+        <button>Log Out</button>
 }
 
 export default Nav
