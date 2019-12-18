828f884bf36407d2980ed1fb5e893e04ce53ee1f
Dev Govindji <friendsofdev94@gmail.com>
2019-12-18 12:54:07 -0600

    yay

--- src/words.js
+++ src/words.js
@@ -1,5 +1,6 @@
 
 let adjectives = ['floppy', 'red', 'crooked', 'rotten', 'slimy', 'broken', 'squishy', 'gloomy', 'grumpy', 'fake', 'sleepy', 'flimsy', 'hairy', 'floating', 'spicy', 'salty', 'crumbling', 'fragile', 'strong', 'weak', 'wet', 'frozen', 'happy']
+
 let nouns = ['dog', 'turtle', 'crow', 'cow', 'sun', 'chair', 'cup', 'tomato', 'backpack', 'tower', 'grape', 'notepad', 'paper', 'pencil', 'car', 'tree', 'fan', 'house', 'person', 'alien', 'boat', 'pretzel', 'egg']
 
 export { adjectives, nouns }
